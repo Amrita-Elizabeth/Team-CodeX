ad9fe7d226328238cac8ab900264adc5ea21f7b6
Amrita-Elizabeth <amritaelizabeth7@gmail.com>
2025-01-20 16:08:32 +0100

    Preprocessing methods-coverting to greyscale&binarisation checked

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a418a5d2fd1571ff26b208913aa22bba92154fe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e559e2bd767c6facc74a19512a6074494c74bc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
