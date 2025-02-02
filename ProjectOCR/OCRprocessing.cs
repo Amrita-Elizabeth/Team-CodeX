571158c550b4077abd3ee0c91eb5fc06ba833f38
Snehamol <snehamolsunny06@gmail.com>
2025-02-02 21:42:04 +0100

    Modified

---
+++ ProjectOCR/OCRprocessing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Tesseract;
+
+// Alias to resolve ambiguity
+using ImagingImageFormat = System.Drawing.Imaging.ImageFormat;
+
+namespace OCRProject.Services
+{
+    public class OcrProcessor
+    {
+        private readonly string _tessDataPath;
+
+        public OcrProcessor(string tessDataPath)
+        {
+            _tessDataPath = tessDataPath;
+        }
+
+        public string ExtractText(Bitmap image)
+        {
+            try
+            {
+                using (var engine = new TesseractEngine(_tessDataPath, "eng", EngineMode.Default))
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        image.Save(memoryStream, ImagingImageFormat.Png);
+                        memoryStream.Position = 0;
+
+                        using (var pix = Pix.LoadFromMemory(memoryStream.ToArray()))
+                        {
+                            using (var page = engine.Process(pix))
+                            {
+                                return page.GetText();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in ExtractText: {ex.Message}");
+                return string.Empty;
+            }
+        }
+    }
+}
+
---
+++ ProjectOCR/Rotation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;  // Required for System.Drawing.Imaging.ImageFormat
+using System.IO;
+using System.Linq;
+using Tesseract;
+
+namespace Preprocessing
+{
+    internal class Rotation
+    {
+        // Method to rotate an image
+        public static Image RotateImage(Image img, float rotationAngle)
+        {
+            Bitmap bmp = new Bitmap(img.Width, img.Height);
+            Graphics gfx = Graphics.FromImage(bmp);
+            gfx.TranslateTransform((float)bmp.Width / 2, (float)bmp.Height / 2);
+            gfx.RotateTransform(rotationAngle);
+            gfx.TranslateTransform(-(float)bmp.Width / 2, -(float)bmp.Height / 2);
+            gfx.DrawImage(img, new Point(0, 0));
+            return bmp;
+        }
+
+        public static Image ApplyThreshold(Image img, int threshold = 128)
+        {
+            Bitmap bmp = new Bitmap(img);
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color pixel = bmp.GetPixel(x, y);
+                    int grayValue = (pixel.R + pixel.G + pixel.B) / 3;
+                    Color newColor = grayValue < threshold ? Color.Black : Color.White;
+                    bmp.SetPixel(x, y, newColor);
+                }
+            }
+            return bmp;
+        }
+
+    }
+}
+
+
